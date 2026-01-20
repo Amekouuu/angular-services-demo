f49788d0cc356afb2e41cb73261474911db80ccd
Amekouuu <mqalberto12@gmail.com>
2026-01-20 08:08:55 +0800

    asd

---
+++ src/app/new-cmp/new-cmp.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-cmp',
+  imports: [],
+  templateUrl: './new-cmp.html',
+  styleUrl: './new-cmp.css',
+})
+export class NewCmp {
+
+}
